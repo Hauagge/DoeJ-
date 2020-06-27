d801cf87d4190889396ac79529be529b2512f0ad
morpheus6556 <ma.usv@hotmail.com>
2020-06-27 16:37:02 -0300

    Create 1593193026235-user.ts

---
+++ back-end/src/shared/Infra/typeorm/migrations/1593193026235-user.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class user1593193026235 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
