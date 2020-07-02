20c421a6d437efb1eac1e4cfa45ce5125c9df503
morpheus6556 <ma.usv@hotmail.com>
2020-07-02 17:33:03 -0300

    Repositorios (User)

---
+++ back-end/src/repositories/CartaoRepositorie.ts
@@ -0,0 +1,46 @@
+import { EntityRepository, Repository } from 'typeorm';
+import Cartao from '../entities/Cartao';
+import User from '../entities/User';
+
+interface ArrayCartao {
+    arrayCartao: Array<Cartao>;
+}
+
+@EntityRepository(Cartao)
+class TransactionsRepository extends Repository<Cartao> 
+{
+    public async createCartao(cartao: Cartao): Promise<void>
+    {
+        await this.save(cartao);
+    }
+
+    public async updateCartao(cartao: Cartao, newCartao: Cartao): Promise<void>
+    {
+        if(newCartao.descricao != null)
+        cartao.descricao = newCartao.descricao;
+        
+        if(newCartao.numero != null)
+        cartao.numero = newCartao.numero;
+        
+        if(newCartao.agencia != null)
+        cartao.agencia = newCartao.agencia;
+
+        if(newCartao.conta != null)
+        cartao.conta = newCartao.conta;
+        
+        await this.save(cartao);
+    }
+
+    public async deleteCartao(cartao: Cartao): Promise<void>
+    {
+        await this.delete(cartao);
+    }
+
+     public async getAccCartoes(user : User): Promise<ArrayCartao>
+     {
+         var list:ArrayCartao = {arrayCartao:new Array<Cartao>()};
+         list.arrayCartao = await this.find({where:{user_ID:user.ID}});
+         return list;
+     }
+
+}
